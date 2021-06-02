bfda9e5852388b8eb687d8b2938cf1a1b8960585
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:00:21 +1000

    Create w3q1.py

---
+++ w3q1.py
@@ -0,0 +1,10 @@
+name = input("Enter name:\n")           # input of a name as a variable.
+
+if name == "george":                    # if statement if person's name is george or frank then greet, otherwise don't greet.
+    print("Hello", f"{name}!")
+elif name == "frank":
+    print("Hello", f"{name}!")
+else:
+    print("No greetings for you!")
+
+# Tomas Franco 101399521
