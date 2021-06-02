852fddd1dc54799f3ec19a2db976640a4bc03c5c
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:00:45 +1000

    Create w4q1.py

---
+++ w4q1.py
@@ -0,0 +1,9 @@
+x = 0
+while x < 26:       # while loop that will only stop once variable x reaches 25
+    print(x)
+    x = x + 1
+
+# Tomas Franco 101399521
+
+# for x in range(26):
+#     print(x)
