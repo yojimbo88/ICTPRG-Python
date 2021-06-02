bc73a7c96e77771014b75857c7f346b51ba180bb
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:00:48 +1000

    Create w4q2.py

---
+++ w4q2.py
@@ -0,0 +1,6 @@
+sum = 0
+for count in range(10,51):      # for loop declaring range between 10 and 50.
+    sum = sum + count           # variable with sum of each number within the above range
+    print(sum)
+
+# Tomas Franco 101399521
