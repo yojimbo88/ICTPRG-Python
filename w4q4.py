a298bc46f92f3b9f1b894649e830919b2667a2b4
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:00:55 +1000

    Create w4q4.py

---
+++ w4q4.py
@@ -0,0 +1,6 @@
+x = 0
+for x in range(26):         # for loop that will only stop once variable x reaches 25
+    print(x, end=", ")      # add end=", " so the count stays on the same line while looping to 25.
+
+# Tomas Franco 101399521
+
