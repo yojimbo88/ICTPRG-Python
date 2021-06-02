9cea26a24a21c875b6b482c805592c65e25500d2
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:00:33 +1000

    Create w3q3.py

---
+++ w3q3.py
@@ -0,0 +1,9 @@
+name = input("Enter name: ")                                    # input of a name as a variable.
+password = input("Enter password: ")                            # input of a password as a variable.
+
+if name == "bob" or "frank" and password == "password1234":     # if statement of name bob or frank and password match the grant access.
+    print("Access Granted!")
+else:                                                           # if not name bob or frank input invalid password then deny access.
+    print("Access Denied!")
+
+# Tomas Franco 101399521
