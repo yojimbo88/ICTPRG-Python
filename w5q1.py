cdcbc91212068bc0f6eaa14446d6c8dc13e02c77
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:01:44 +1000

    Create w5q1.py

---
+++ w5q1.py
@@ -0,0 +1,9 @@
+values = [66,43,1,6,2,99,4]    # list with some numbers.
+
+x  = 0
+while x < len(values):         # create a loop determining to only print if any number less than 10 then output.
+    if values[x] < int(10):
+        print(values[x])
+    x += 1
+
+# Tomas Franco 101399521
