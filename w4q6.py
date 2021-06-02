915c3612a8a1d2b7f11fb5cb74da5813bb31d835
yojimbo88 <tomashfranco@gmail.com>
2021-06-02 14:01:05 +1000

    Create w4q6.py

---
+++ w4q6.py
@@ -0,0 +1,7 @@
+
+for count in range(0,5):            # nested loop telling program to print 5 lines of x 5 times in a row.
+    for x in range(0,5):
+        print("x", end='' )
+    print("")
+
+# Tomas Franco 101399521
